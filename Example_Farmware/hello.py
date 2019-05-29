5c1ffe342bd10716ece6179186b2f9e941693b24
Lukas Creutzburg <lukas.creutzburg@gmail.com>
2019-05-29 21:38:56 +0200

    4

--- Example_Farmware/hello.py
+++ Example_Farmware/hello.py
@@ -5,6 +5,10 @@
 A simple Farmware example that tells FarmBot to log a new message.
 '''
 
+from farmware_tools import app
 from farmware_tools import device
 
 device.log(message='Hello Farmware!', message_type='success')
+
+plants = app.get_plants()
+device.log(message=plants, message_type='success')
